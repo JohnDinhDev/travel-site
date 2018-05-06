85dc9c05cde5f66d51428ef599fb8623030469db
John Dinh <JohnDuyDinh@Gmail.com>
2018-05-05 21:57:41 -0400

    Complete Sticky Header

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -2,12 +2,21 @@
 
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
     this.$siteHeader = $('.site-header');
     this.$headerTriggerElement = $('.large-hero__title');
     this.createHeaderWaypoint();
+    this.$pageSections = $('.page-section');
+    this.$headerLinks = $('.primary-nav a');
+    this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.$headerLinks.smoothScroll();
   }
 
   createHeaderWaypoint() {
@@ -21,6 +30,39 @@ class StickyHeader {
           _this.$siteHeader.removeClass('site-header--dark');
         }
       },
+    });
+  }
+
+  createPageSectionWaypoints() {
+    var _this = this;
+    var that = _this;
+    this.$pageSections.each(function () {
+      var _this = this;
+      new Waypoint({
+        element: _this,
+        handler: function (direction) {
+          if (direction == 'down') {
+            var matchingHeaderLink = _this.getAttribute('data-matching-link');
+            that.$headerLinks.removeClass('is-current-link');
+            $(matchingHeaderLink).addClass('is-current-link');
+          }
+        },
+
+        offset: '18%',
+      });
+
+      new Waypoint({
+        element: _this,
+        handler: function (direction) {
+          if (direction == 'up') {
+            var matchingHeaderLink = _this.getAttribute('data-matching-link');
+            that.$headerLinks.removeClass('is-current-link');
+            $(matchingHeaderLink).addClass('is-current-link');
+          }
+        },
+
+        offset: '-40%',
+      });
     });
   }
 }
